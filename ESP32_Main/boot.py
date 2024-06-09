9c0d87c9e7b0abecc251026a3a793aa43c0c7c10
Xeift <xeift@localhost.localdomain>
2024-06-09 20:24:40 +0800

    initial commit

---
+++ ESP32_Main/boot.py
@@ -0,0 +1,40 @@
+'''
+TODO:
+新增 API，連接眼鏡
+自動更新溫溼度
+黑暗模式切換
+導出 excel
+CSS 最佳化
+''' 
+
+try: import usocket as socket
+except: import socket
+from machine import Pin
+import network
+import time
+import esp
+import dht
+
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+# ssid = 'HUAWEI-B525-03C8-cccluke'
+# password = 'passwordcccluke'
+
+ssid = 'Xeift'
+password = '13241324'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True) # 連上 Wi-Fi
+station.connect(ssid, password)
+
+while station.isconnected() == False: print('WiFi連接中')
+
+print('WiFi連接成功')
+print(station.ifconfig())
+
+led = Pin(13, Pin.OUT) # LED 腳位
+dht11 = dht.DHT11(Pin(14)) # 溫溼度感測器腳位
---
+++ ESP32_Main/main.py
@@ -0,0 +1,131 @@
+from machine import Pin
+from time import sleep
+import dht
+
+
+def web_page(temp, hum, temp_f):  # 顯示網頁 html 內容
+    if led.value() == 1:
+        gpio_state = "ON"
+    else:
+        gpio_state = "OFF"
+
+    html = """
+    <html>
+    <head>
+        <title>ESP Web Server</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="data:,">
+        <style>
+            html {
+                font-family: Helvetica;
+                display: inline-block;
+                margin: 0px auto;
+                text-align: center;
+            }
+            h1 {
+                color: #0F3376;
+                padding: 2vh;
+            }
+            p {
+                font-size: 1.5rem;
+            }
+            .button {
+                display: inline-block;
+                background-color: #e7bd3b;
+                border: none;
+                border-radius: 4px;
+                color: white;
+                padding: 16px 40px;
+                text-decoration: none;
+                font-size: 30px;
+                margin: 2px;
+                cursor: pointer;
+            }
+            .button2 {
+                background-color: #4286f4;
+            }
+            table {
+                margin: 20px auto; /* Center the table */
+                border-collapse: collapse;
+                width: 50%;
+                font-size: 1.2rem;
+            }
+            table, th, td {
+                border: 1px solid black;
+            }
+            th, td {
+                padding: 12px;
+                text-align: center; /* Center text in table cells */
+            }
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+        <scrip>
+          setInterval(updateData, 5000);
+        </script>
+    </head>
+    <body>
+        <h1>ESP Web Server</h1>
+        <p>GPIO state: <strong>""" + str(gpio_state) + """</strong></p>
+        <p><a href="/?led=on"><button class="button">ON</button></a></p>
+        <p><a href="/?led=off"><button class="button button2">OFF</button></a></p>
+        <table>
+        <tr>
+          <td>Celsius temperature</td>
+          <td>""" + str(temp) + """</td>
+      </tr>
+        <tr>
+          <td>Humifity</td>
+          <td>""" + str(hum) + """</td>
+      </tr>
+        <tr>
+          <td>Fahrenheit temperature</td>
+          <td>""" + str(temp_f) + """</td>
+      </tr>
+      </table>
+    </body>
+    </html>
+    """
+
+    return html
+
+
+# Web Server 主程式
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 80))
+s.listen(5)  # 監聽 80 port
+
+
+while True:
+    conn, addr = s.accept()  # 當網頁端連接時
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)  # 顯示收到的資料
+    led_on = request.find('/?led=on')  # 確定目前 led 燈是否開啟
+    led_off = request.find('/?led=off')
+    if led_on == 6:  # 如果開啟就顯示 LED ON
+        print('LED ON')
+        led.value(1)
+    if led_off == 6:
+        print('LED OFF')
+        led.value(0)
+
+    sleep(2)
+    sensor = dht11
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    temp_f = temp * (9/5) + 32.0
+    print('Temperature: %3.1f C' % temp)
+    print('Temperature: %3.1f F' % temp_f)
+    print('Humidity: %3.1f %%' % hum)
+
+    response = web_page(temp, hum, temp_f)  # 顯示網頁
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)  # return 網頁
+    conn.close()
