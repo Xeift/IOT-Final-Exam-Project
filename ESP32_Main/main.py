0a803e80726e64f358f0a2e3c0b2ef5bcb631309
Xeift <xeift@localhost.localdomain>
2024-06-10 20:27:53 +0800

    add live chart

--- ESP32_Main/main.py
+++ ESP32_Main/main.py
@@ -4,6 +4,7 @@ from machine import Pin
 from time import sleep
 
 
+
 '''    顯示網頁    '''
 def web_page():
     if led.value() == 1:
@@ -47,7 +48,7 @@ def web_page():
                     background-color: #4286f4;
                 }
                 table {
-                    margin: 20px auto; /* Center the table */
+                    margin: 20px auto;
                     border-collapse: collapse;
                     width: 50%;
                     font-size: 1.2rem;
@@ -57,25 +58,103 @@ def web_page():
                 }
                 th, td {
                     padding: 12px;
-                    text-align: center; /* Center text in table cells */
+                    text-align: center;
                 }
                 th {
                     background-color: #f2f2f2;
                 }
+                #chartContainer {
+                    width: 80%;
+                    margin: 20px auto;
+                }
             </style>
+            <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1"></script>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@^1"></script>
             <script>
-              setInterval(updateData, 2200);
+              var tempData = [];
+              var humData = [];
+              var labels = [];
+              var maxDataPoints = 20;
+
+              setInterval(updateData, 1000);
 
               function updateData() {
-                fetch(\'http://""" + station.ifconfig()[0] + """/api/get-temp-hum\')
+                fetch('http://""" + station.ifconfig()[0] + """/api/get-temp-hum')
                   .then(response => response.json())
                   .then(data => {
                     document.getElementById('temp').innerText = data.temp;
                     document.getElementById('hum').innerText = data.hum;
                     document.getElementById('temp_f').innerText = data.temp_f;
+
+                    var now = new Date();
+                    labels.push(now);
+                    tempData.push(data.temp);
+                    humData.push(data.hum);
+
+                    if (labels.length > maxDataPoints) {
+                      labels.shift();
+                      tempData.shift();
+                      humData.shift();
+                    }
+
+                    window.tempChart.update();
+                    window.humChart.update();
                   })
                   .catch(error => console.error('Error fetching data:', error));
               }
+
+              window.onload = function() {
+                var ctxTemp = document.getElementById('tempChart').getContext('2d');
+                var ctxHum = document.getElementById('humChart').getContext('2d');
+                window.tempChart = new Chart(ctxTemp, {
+                  type: 'line',
+                  data: {
+                    labels: labels,
+                    datasets: [{
+                      label: 'Temperature (°C)',
+                      data: tempData,
+                      borderColor: 'rgba(255, 99, 132, 1)',
+                      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                      fill: true
+                    }]
+                  },
+                  options: {
+                    scales: {
+                      x: {
+                        type: 'time',
+                        time: {
+                          unit: 'second'
+                        }
+                      }
+                    }
+                  }
+                });
+
+                window.humChart = new Chart(ctxHum, {
+                  type: 'line',
+                  data: {
+                    labels: labels,
+                    datasets: [{
+                      label: 'Humidity (%)',
+                      data: humData,
+                      borderColor: 'rgba(54, 162, 235, 1)',
+                      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                      fill: true
+                    }]
+                  },
+                  options: {
+                    scales: {
+                      x: {
+                        type: 'time',
+                        time: {
+                          unit: 'second'
+                        }
+                      }
+                    }
+                  }
+                });
+              };
             </script>
         </head>
         <body>
@@ -97,6 +176,10 @@ def web_page():
               <td id="temp_f">""" + str(temp_f) + """</td>
             </tr>
             </table>
+            <div id="chartContainer">
+                <canvas id="tempChart"></canvas>
+                <canvas id="humChart"></canvas>
+            </div>
         </body>
     </html>
     """
@@ -127,19 +210,18 @@ def api_response():
 
 '''    Web Server 主程式    '''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 新增 SO_REUSEADDR，解決 address in use 的報錯
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 80))
-s.listen(5)  # 監聽 80 port
-s.setblocking(False)  # 設置為非阻塞模式
+s.listen(5)
+s.setblocking(False)
 
 
 '''    ESP32 主程式    '''
 while True:
     try:
         update_dht11_data()
-    
 
-        conn, addr = s.accept()  # 當網頁端連接時
+        conn, addr = s.accept()
         print('------------------------------')
         print('客戶端 %s 已連線' % str(addr))
         print('------------------------------\n')
@@ -147,40 +229,30 @@ while True:
         request = conn.recv(1024)
         request = str(request)
 
-
         if request.find('/?led=on') == 6:
             led.value(1)
-            
         elif request.find('/?led=off') == 6:
             led.value(0)
-
-        elif request.find('/api/get-temp-hum') == 6: #
+        elif request.find('/api/get-temp-hum') == 6:
             response = api_response()
-            conn.send(response)
+            conn.send(response.encode())
             conn.close()
-            
         else:
             temp = dht11.temperature()
             temp_f = temp * (9/5) + 32.0
             hum = dht11.humidity()
             response = web_page()
-            conn.send('HTTP/1.1 200 OK\n')
-            conn.send('Content-Type: text/html\n')
-            conn.send('Connection: close\n\n')
-            conn.sendall(response) # return 網頁
+            conn.send('HTTP/1.1 200 OK\n'.encode())
+            conn.send('Content-Type: text/html\n'.encode())
+            conn.send('Connection: close\n\n'.encode())
+            conn.sendall(response.encode())
             conn.close()
 
-
-        '''    處理例外情況    '''
     except OSError as e:
-        
-        if e.args[0] == 11: # EAGAIN，處理沒有客戶端連接時的狀況
-            print('sleep!')
-            sleep(0.001) # 短暫休眠，避免CPU佔用過高
+        if e.args[0] == 11:
+            sleep(0.001)
             continue
-
-        elif e.args[0] == 116: # ETIMEDOUT，處理 DHT11 讀不到數值時的狀況
+        elif e.args[0] == 116:
             print('timeout!')
-            sleep(2.1) # 暫停一下，等待 DHT11 恢復正常
+            sleep(2.1)
             continue
-
